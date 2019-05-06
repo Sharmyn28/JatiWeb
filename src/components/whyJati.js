b3e17b93f61c34195f056780f3dab64f8837fb54
Milagros Rodriguez <milagros@krowdy.co>
2019-05-06 18:21:08 -0500

    [update] adding css and images to whyJati Section

--- src/components/whyJati.js
+++ src/components/whyJati.js
@@ -2,34 +2,42 @@ import React from 'react'
 import { Row, Col} from 'antd';
 import "antd/dist/antd.css"
 
+// importing images
+import teamIcon from '../assets/images/home/whyJatiIcons/Jati-equipo-Logo.png'
+import peruvianFlagIcon from '../assets/images/home/whyJatiIcons/Jati-bandera-Logo.png'
+import buildingIcon from '../assets/images/home/whyJatiIcons/Jati-casa-Logo.png'
+
 const ReasonsJati = () => {
   return (
-    <div>
+    <div className='mtop80'>
       <Row>
-        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-          <p> UN EQUIPO MULTIDISCIPLINARIO </p>
+        <Col xs={{ span: 22, offset: 1 }} sm= {{span: 22, offset: 1}} md={{span: 4, offset: 4}} lg={{span: 4, offset: 4}}  className='col-itemBox'>
+          <img className='img--responsive'  src={teamIcon} alt='teamIcon' />
+          <p className='col-itemBox-txt'> UN EQUIPO MULTI DISCIPLINARIO </p>
         </Col>
-
-        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-          <p> UNICO EQUIPO PERUANO ENTRE LOS FINALISTAS </p>
+        <Col  xs={{ span: 22, offset: 1 }} sm= {{span: 22, offset: 1}} md={{span: 4, offset: 2}} lg={{span: 4, offset: 2}} className='col-itemBox'>
+          <img className='img--responsive' src={peruvianFlagIcon} alt='flagIcon' />
+          <p className='col-itemBox-txt'> UNICO EQUIPO PERUANO ENTRE LOS FINALISTAS </p>
         </Col>
-        <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-          <p> CONSTRUYENDO UN FUTURO MEJOR PARA LAS FAMILIAS PERUANAS </p>
+        <Col  xs={{ span: 22, offset: 1 }} sm= {{span: 22, offset: 1}} md={{span: 4, offset: 2}} lg={{span: 4, offset: 2}} className='col-itemBox'>
+          <img className='img--responsive' src={buildingIcon} alt='houseIcon' />
+          <p className='col-itemBox-txt'> CONSTRUYENDO PARA LAS FAMILIAS PERUANAS </p>
         </Col>
       </Row>
     </div>
   )
 }
 
+// xs={24} sm={24} md={8} lg={8} xl={8}
+// type="flex" justify="space-around"  span={4}
 const WhyJati = () => {
   return (
-    <div>
+    <div className='whyJatiSection'>
       <Row>
-      imagen
-        <h2> ¿Por qué apoyar a Játi? </h2>
+        <h1 className='titles-description txtWhite txt-center'> ¿Por qué apoyar a Játi? </h1>
       </Row>
       <Row>
-        <p> Segunda vez consecutiva que el equipo de la <strong>UNI</strong> queda como uno de los finalistas del concurso <strong>Solar Decathlon</strong>. </p>
+        <p className='txtWhite txt-center subtitle-p'> Segunda vez consecutiva que el equipo de la <strong>UNI</strong> queda como uno de los finalistas del concurso <strong>Solar Decathlon</strong>. </p>
       </Row>
       <ReasonsJati />
     </div>
