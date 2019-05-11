60582a66a886a17824c6c9ce2cb662e5f706ec2b
Milagros Rodriguez <milagros@krowdy.co>
2019-05-11 18:58:29 -0500

    [update] adding pics to carousel Rspaldo

--- src/components/Respaldo.js
+++ src/components/Respaldo.js
@@ -34,10 +34,40 @@ const RespaldoSlider = () => {
         </Row>
       </div>
       <div>
-        Carrousel 2
+        <Row>
+          <Col xs={13} sm={13} md={{span: 12, offset:2}} lg={{span: 12, offset:2}} xl={{span: 12, offset:2}} >
+            <img src={respaldo2} className='img--responsive' alt='respaldo' />
+          </Col>
+          <Col xs={{span: 10, offset:1}} sm={{span: 10, offset:1}} md={{span: 8, offset:1}} lg={{span: 7, offset:1}} xl={{span: 7, offset:1}}>
+            <Row type="flex" align="middle">
+              <div>
+                <p className='txtBlue bold'> MIGUEL SALVATIERRA </p>
+                <small className='txtGreen bold'> CEO DE SOLA INDUSTRY </small>
+              </div>
+              <div>
+                <p className='txtGrey respaldo-txt'> Decidimos apoyar al Proyecto Jati porque no solo tiene un modelo de vivienda sostenible que es de mucha utilidad para los pobladores de las zonas de la amazonía peruana sino también que quieren realizar un gran cambio en nuestra sociedad de hoy en día. </p>
+              </div>
+            </Row>
+          </Col>
+        </Row>
       </div>
       <div>
-        Carrousel 2
+        <Row>
+          <Col xs={13} sm={13} md={{span: 12, offset:2}} lg={{span: 12, offset:2}} xl={{span: 12, offset:2}} >
+            <img src={respaldo2} className='img--responsive' alt='respaldo' />
+          </Col>
+          <Col xs={{span: 10, offset:1}} sm={{span: 10, offset:1}} md={{span: 8, offset:1}} lg={{span: 7, offset:1}} xl={{span: 7, offset:1}}>
+            <Row type="flex" align="middle">
+              <div>
+                <p className='txtBlue bold'> MIGUEL SALVATIERRA </p>
+                <small className='txtGreen bold'> CEO DE SOLA INDUSTRY </small>
+              </div>
+              <div>
+                <p className='txtGrey respaldo-txt'> Decidimos apoyar al Proyecto Jati porque no solo tiene un modelo de vivienda sostenible que es de mucha utilidad para los pobladores de las zonas de la amazonía peruana sino también que quieren realizar un gran cambio en nuestra sociedad de hoy en día. </p>
+              </div>
+            </Row>
+          </Col>
+        </Row>
       </div>
     </Carousel>
   )
--- src/views/Home.js
+++ src/views/Home.js
@@ -16,30 +16,36 @@ import Contact from '../components/Contact'
 import logoJati from  '../assets/images/home/Jati-Logo.png'
 import map from '../assets/images/home/Seccion1-map.png'
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
 const HomeLayout = () => {
+  const state = {
+    collapsed: false,
+  }
+
   return (
     <Layout className="layout">
 
       <Header className='mainBlueBg header'>
         <Row>
-          <Col xs={10} sm={10} md={6} lg={6} xl={6}>
-            <Avatar src={logoJati} style={{width: '80px', height: '80px'}} />
+          <Col xs={8} sm={10} md={6} lg={6} xl={6}>
+            <Avatar src={logoJati} className='logo-menu' />
           </Col>
-          <Col xs={14} sm={14} md={18} lg={18} xl={18}>
-            <Menu
-              mode="horizontal"
-              defaultSelectedKeys={['1']}
-              style={{ lineHeight: '76px', float: 'right', color: '#fff' }}
-              className = 'mainBlueBg'
-            >
-              <Menu.Item key="1">CONOCENOS</Menu.Item>
-              <Menu.Item key="2">NOTICIAS Y EVENTOS</Menu.Item>
-              <Menu.Item key="3">RESPALDO</Menu.Item>
-              <Menu.Item key="4">DONACIONES</Menu.Item>
-              <Menu.Item key="5">CONTACTOS</Menu.Item>
-            </Menu>
+          <Col xs={16} sm={14} md={18} lg={18} xl={18}>
+
+              <Menu
+                mode="horizontal"
+                defaultSelectedKeys={['1']}
+                className = 'mainBlueBg content-menu'
+                defaultSelectedKeys={['1']}
+              >
+                <Menu.Item key="1">CONOCENOS</Menu.Item>
+                <Menu.Item key="2">NOTICIAS Y EVENTOS</Menu.Item>
+                <Menu.Item key="3">RESPALDO</Menu.Item>
+                <Menu.Item key="4">DONACIONES</Menu.Item>
+                <Menu.Item key="5">CONTACTOS</Menu.Item>
+              </Menu>
+
           </Col>
         </Row>
       </Header>
