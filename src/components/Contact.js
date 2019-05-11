318260602d9202f910c2fc8c8c6a5bbaff92a661
Milagros Rodriguez <milagros@krowdy.co>
2019-05-11 02:12:44 -0500

    [update] adding form to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,21 +2,61 @@ import React from 'react'
 import { Row, Col, Button, Form, Icon, Input } from 'antd'
 import "antd/dist/antd.css"
 
+// importing images
+import logo from '../assets/images/home/Jati-Logo-big.png'
+
+const { TextArea } = Input
+
+const FormContact = () => {
+  return (
+    <Form  className="login-form contactForm">
+      <Form.Item>
+        <Row>
+          <Col xs={24} sm={24} md={{span: 11}} lg={{span: 11}} xl={{span: 11}}>
+            <Input type='text' placeholder="Nombre" />
+          </Col>
+          <Col xs={24} sm={24} md={{span: 12, offset:1}} lg={{span: 12, offset:1}} xl={{span: 12, offset:1}}>
+            <Input type='text' placeholder='Apellido' />
+          </Col>
+        </Row>
+      </Form.Item>
+      <Form.Item>
+        <Input type="email" placeholder="Correo electrónico" />
+      </Form.Item>
+      <Form.Item>
+        <Input  type="number" placeholder="Celular" maxLength={9} />
+      </Form.Item>
+      <Form.Item>
+        <TextArea placeholder="Preguntas" autosize={{ minRows: 3, maxRows: 6 }} />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" size='large' className="login-form-button greenBg">
+          Enviar
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
 
 const Contact = () => {
   return (
-    <div>
+    <div className='contactSection'>
       <Row>
-        <Col>
-          imagen
-          <p> Decidimos apoyar el Proyecto Jati porque no solo tiene un modelo de vivienda sostenible que es de mucha utilidad para los pobladores de las zonas de la amazonía peruana sino también que quieren realizar un gran cambio en nuestra sociedead de hoy en día.</p>
+        <Col md={{span: 8, offset:2}} lg={{span: 8, offset:2}} xl={{span: 8, offset:2}} className='logo-contact'>
+          <div className='complete-center-div'>
+            <div>
+              <img src={logo} className='img--responsive' alt='logo' />
+            </div>
+          </div>
+          <Col xs={24} sm={24} md={{span:14, offset:5}} lg={{span:14, offset:5}} xl={{span:14, offset:5}} className='contact-info'>
+            <small className='txtWhite'> Decidimos apoyar el Proyecto Jati porque no solo tiene un modelo de vivienda sostenible que es de mucha utilidad para los pobladores de las zonas de la amazonía peruana sino también que quieren realizar un gran cambio en nuestra sociedead de hoy en día.</small>
+          </Col>
         </Col>
 
-        <Col>
-          Mantente Informado de nuestras novedades:
-
-
-
+        <Col xs={{span: 22, offset:1}} sm={{span: 22, offset:1}} md={{span: 10, offset:1}} lg={{span: 10, offset:1}} xl={{span: 10, offset:1}}>
+          <p className='txtWhite bold txt-center'>Mantente Informado de nuestras novedades:</p>
+          <FormContact />
         </Col>
       </Row>
     </div>
@@ -24,7 +64,7 @@ const Contact = () => {
 }
 
 export default Contact
-
+//
 // <Form  className="login-form">
 //   <Form.Item>
 //     // {getFieldDecorator( {
