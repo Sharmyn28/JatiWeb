52994fbe9a9a9fa2702f416a4e7dc95b52918307
Milagros Rodriguez <milagros@krowdy.co>
2019-05-11 02:13:54 -0500

    [update] adding carousel images to first section

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,6 +4,9 @@ import "antd/dist/antd.css"
 
 // importing pics for Carousel
 import slider1 from '../assets/images/home/carousel/Slider-1.png'
+import slider2 from '../assets/images/home/carousel/carousel-group2.jpg'
+import slider3 from '../assets/images/home/carousel/carousel-group3.jpg'
+import slider4 from '../assets/images/home/carousel/carousel-4.jpg'
 
 const HomeCarousel = () => {
   return (
@@ -18,13 +21,13 @@ const HomeCarousel = () => {
         <img src={slider1} className='carousel-img' alt='sliderImg' />
       </div>
       <div>
-        <img src={slider1} className='carousel-img' alt='sliderImg' />
+        <img src={slider2} className='carousel-img' alt='sliderImg2' />
       </div>
       <div>
-        <img src={slider1} className='carousel-img' alt='sliderImg' />
+        <img src={slider3} className='carousel-img' alt='sliderImg3' />
       </div>
       <div>
-        <img src={slider1} className='carousel-img' alt='sliderImg' />
+        <img src={slider4} className='carousel-img' alt='sliderImg' />
       </div>
   </Carousel>
   )
